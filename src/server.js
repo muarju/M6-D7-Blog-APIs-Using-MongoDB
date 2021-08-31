e0a2767be11a218ca3b98828fd0c4a4951af79a6
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-31 14:51:40 +0100

    init project files

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import mongoose from 'mongoose'
+
+import blogsRouter from "./services/blogs/index.js"
+import {badRequestErrorHandler, catchAllErrorHandler, notFoundErrorHandler} from './errorHandlers.js'
+
+const server = express()
+
+const port = process.env.PORT || 3001
+
+// ******************** MIDDLEWARES ******************
+
+server.use(express.json())
+
+// ******************* ROUTES ***********************
+
+server.use("/blogPosts", blogsRouter)
+
+
+// ******************* ERROR HANDLERS ******************
+
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(catchAllErrorHandler)
+
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+  console.log('Successfully connected to mongo!')
+  server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log("Server is running on port ", port)
+  })
+})
+
+mongoose.connection.on("error", err => {
+  console.log("MONGO ERROR: ", err)
+})
