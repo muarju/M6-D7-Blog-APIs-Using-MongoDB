da061784fd8200c0f1d373269721bc40f4ac7bc5
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-02 16:39:48 +0100

    authors CRUD and Login Done

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import createError from 'http-errors'
-
+import bcrypt from 'bcryptjs'
 import authorModel from './schema.js'
 
 const authorsRouter = express.Router()
 
 authorsRouter.post("/", async(req,res,next) => {
   try {
+    const password= req.body.password
+    const passwordHash = bcrypt.hashSync(password, 10);
+    const email= req.body.email
 
-    const newBlog = new authorModel(req.body) // here happens validation of the req.body, if it's not ok mongoose will throw a "ValidationError"
-    const {_id} = await newBlog.save()
+    const newAuthor = new authorModel({...req.body,email: email.toLowerCase(), password: passwordHash}) // here happens validation of the req.body, if it's not ok mongoose will throw a "ValidationError"
+    const {_id} = await newAuthor.save()
 
     res.status(201).send({_id})
     
@@ -18,30 +21,46 @@ authorsRouter.post("/", async(req,res,next) => {
   }
 })
 
-authorsRouter.get("/", async(req,res,next) => {
+authorsRouter.post("/login", async(req,res,next) => {
   try {
-    
-    const blogPosts = await authorModel.find({})
-
-    res.send(blogPosts)
+    const {email,password}=req.body;
+    const data = await authorModel.findOne(
+      { "email": email.toLowerCase() },
+    );
+     // check account found and verify password
+    if (!data || !bcrypt.compareSync(password, data.password)) {
+      res.status(200).send("authentication failed");
+    } else {
+      // authentication successful
+      res.send(data);
+    }
     
   } catch (error) {
     next(error)
   }
 })
+
+authorsRouter.get("/", async(req,res,next) => {
+  try {
+    const authors = await authorModel.find({})
+    res.send(authors)
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 authorsRouter.get("/:authorId", async(req,res,next) => {
   try {
 
     const authorId = req.params.authorId
+    const authors = await authorModel.findById(authorId) // similar to findOne()
 
-    const blogPosts = await authorModel.findById(authorId) // similar to findOne()
-
-    if(blogPosts){
-
-      res.send(blogPosts)
-
+    if(authors){
+      res.send(authors)
     } else {
-      next(createError(404, `Blog with id ${authorId} not found!`))
+      next(createError(404, `Author with id ${authorId} not found!`))
     }
     
   } catch (error) {
@@ -53,14 +72,14 @@ authorsRouter.put("/:authorId", async(req,res,next) => {
   try {
     const authorId = req.params.authorId
 
-    const modifiedBlog = await authorModel.findByIdAndUpdate(authorId, req.body, {
+    const modifiedAuthor = await authorModel.findByIdAndUpdate(authorId, req.body, {
       new: true // returns the modified blog
     })
 
-    if(modifiedBlog){
-      res.status(202).send(modifiedBlog)
+    if(modifiedAuthor){
+      res.status(202).send(modifiedAuthor)
     } else {
-      next(createError(404, `User with id ${authorId} not found!`))
+      next(createError(404, `author with id ${authorId} not found!`))
     }
   } catch (error) {
     next(error)
@@ -71,38 +90,14 @@ authorsRouter.delete("/:authorId", async(req,res,next) => {
   try {
     const authorId = req.params.authorId
 
-    const deletedBlog = await authorModel.findByIdAndDelete(authorId)
+    const deletedAuthor = await authorModel.findByIdAndDelete(authorId)
 
-    if(deletedBlog){
+    if(deletedAuthor){
       res.status(204).send()
     } else {
-      next(createError(404, `User with id ${authorId} not found!`))
+      next(createError(404, `Author with id ${authorId} not found!`))
     }
   } catch (error) {
-    next(error)
-  }
-})
-
-authorsRouter.delete("/:authorId/comments/:commentId", async(req,res,next) => {
-  try {
-    const { authorId, commentId } = req.params
-
-    const blog = await authorModel.findByIdAndUpdate(
-      authorId,
-      {
-        $pull: {
-          Comments: { _id: commentId },
-        },
-      },
-      {
-        new: true,
-      }
-    )
-    res.status(200).send(blog)
-
-  } catch (error) {
-    res.status(500)
-    console.log(error)
     next(error)
   }
 })
--- src/services/authors/schema.js
+++ src/services/authors/schema.js
@@ -4,6 +4,8 @@ const {Schema, model} = mongoose
 
 const authorSchema = new Schema({
     name: {type:String, required: true},
+    email: {type:String, required: true},
+    password: {type:String, required: true},
     avatar: {type:String, required: true},
     }, {
         timestamps: true
